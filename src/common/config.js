3126b4d84ad21d9f0ecb028ef56fe516682dfc66
may <ayceay@gmail.com>
2024-11-06 23:54:29 +0300

    express framework+postgresql+swagger

---
+++ src/common/config.js
@@ -0,0 +1,27 @@
+//load all the environment variables
+
+const { json } = require('express');
+const fs = require('fs');
+const path = require('path');
+const desiredPath = path.resolve(__dirname, './.env');
+console.log("desired path: "+desiredPath );
+
+console.log("i am here-1");
+
+if (fs.existsSync(desiredPath)) {
+  console.log("i am here-2");
+    // Load environment variables from .env file
+    require('dotenv').config({path: desiredPath})
+}
+
+const envs = {
+  employeeDatabaseUser : process.env.EMPLOYEE_DATABASE_USERNAME,
+  employeeDatabasePassword : process.env.EMPLOYEE_DATABASE_PASSWORD,
+  employeeDatabaseHost : process.env.EMPLOYEE_DATABASE_HOST,
+  employeeDatabasePort : process.env.EMPLOYEE_DATABASE_PORT,
+  employeeDatabaseName : process.env.EMPLOYEE_DATABASE_NAME,
+};
+console.log("===================> " + envs.employeeDatabaseName);
+console.log("===================> " + envs.employeeDatabasePassword);
+
+module.exports = envs;
